94ffd8684f120ea39962398daea99f4a11313e6a
gyachdav <gyachdav@rostlab.org>
2013-05-01 23:33:15 -0400

    added events control to menu items

--- control/ppres-control.js
+++ control/ppres-control.js
@@ -24,125 +24,117 @@ var APP = (function() {
 			"PROFtmb"];
 
 
+	var drawFeatureViewer = function() {
+		FEATURE_VIEWER.init({
+			targetDiv: jQuery("#FeatureViewer"),
+			dataObj: mainObj
+		});
+
+		var features_array = [];
+		FEATURE_VIEWER.setFeaturesArray(jQuery.map(providers, function(provider, index) {
+			var track, feature_properties;
+			track = new Track();
+			if (provider == "ISIS") track.setShiftBottomLine(Track.NO_BOTTOMLINE_SHIFT);
+			else track.setPosition(FEATURE_VIEWER.getCurrentBottom());
+
+			var feature_group = mainObj.getFeatureByProvider(mainObj.getFeatureTypeGroup(), provider);
+			if (!feature_group) return null;
+			var feature_type = (feature_group.type) ? feature_group.type : "";
+			feature_properties = mainObj.getFeatureLocations(feature_group);
+
+
+			if (!feature_properties) return null;
+			if (feature_properties) i = feature_properties.length;
+			while (i--) {
+				var feature;
+				if (typeof Feature[provider] !== 'undefined') {
+					Feature[provider].prototype = new Feature();
+					feature = new Feature[provider](feature_properties[i], provider, feature_type);
+				} else {
+					feature = new Feature().init(feature_properties[i], provider, feature_type);
+				}
+				track.addFeature(feature);
+			}
+			FEATURE_VIEWER.addTrack(track);
+			return track.getTrack();
+		}));
+
+
+		// Add alignment
+		FEATURE_VIEWER.setFeaturesArray(jQuery.map(mainObj.getAlignmentLocations(), function(target, index) {
+			var track = new Track(1, 1);
+			track.setPosition(FEATURE_VIEWER.getCurrentBottom());
+			Feature.Alignment.prototype = new Feature();
+			feature = new Feature.Alignment(target, 'blast', 'alignmnet');
+			track.addFeature(feature);
+			FEATURE_VIEWER.addTrack(track);
+
+			return track.getTrack();
+			//iterate through array or object
+		}));
+
+		FEATURE_VIEWER.draw();
+	};
+
+	var drawSubcellLoc = function() {
+		var nav_div = jQuery("<div id='_subcell_nav' />").appendTo(jQuery("#cell"));
+
+		var content_div = jQuery("<div id='_subcell_cntnt'/>").appendTo(jQuery("#cell"));
+		var list = jQuery('<ul/>');
+		list.addClass("nav nav-pills");
+
+
+		list.append('<li class="disabled"><a href="#">Domains:</a> </li>')
+
+		var domains = ["arch", "bact", "euka", "plant", "animal", "proka"];
+		for (var i in domains) {
+			var _curr_div;
+			var _curr_li = jQuery('<li><a href=#>' + SUBCELL_VIEW.getDomainFullName(domains[i]) + '</a></li>');
+			var prediction = mainObj.getSubCellLocations(domains[i]);
+			if (!prediction) _curr_div = jQuery("<div />").text("Data unavailable");
+			else _curr_div = SUBCELL_VIEW.localisationDiv(prediction);
+
+			if (domains[i] == 'euka') {
+				_curr_div.show();
+				_curr_li.addClass('active');
+			} else _curr_div.hide();
+			list.append(_curr_li);
+			content_div.append(_curr_div);
+		}
+		nav_div.append(list);
+	};
+
+	var drawAlignmentTable = function() {
+		ALI_VIEW.draw(mainObj.getAlignmentLocations(), jQuery("#alignments"));
+	};
+
+	var drawSummaryTable = function() {
+		// TODO move modal activation code from this control
+		jQuery("#summary_container").append("<div class='summary  left' />");
+		jQuery(".summary").append("<h3>Summary</h3>");
+		var table = jQuery("<table/>");
+		table.addClass("table table-striped");
+		if (_rec_name = mainObj.getAlignmentsByDatabaseTopMatch('Swiss-Prot')) table.append("<tr><td>Recommended Name</td><td>" + _rec_name + "</td></tr>");
+		table.append("<tr><td>Sequence Length</td><td>" + mainObj.getSequence().length + "</td></tr>");
+		table.append("<tr><td>Number of Aligned Proteins</td><td><a href='#myModal' role='button' data-toggle='modal'>" + mainObj.getAlignmentsCount() + "</a></td></tr>");
+		table.append("<tr><td>Number of Matched PDB Structures</td><td>" + mainObj.getAlignmentsByDatabase('pdb') + "</td></tr>");
+		jQuery(".summary").append(table);
+	};
+
+	var drawAAConsistency = function() {
+		jQuery("#summary_container").append("<div id='aa-consistency' class='summary  left' />");
+		jQuery("#aa-consistency").append("<h3>Amino Acid composition</h3>");
+		PIE_CHART.toPieData(mainObj.getAAComposition()).drawPieChart('aa-consistency');
+	};
+
+	var drawSSConsistency = function() {
+		jQuery("#summary_container").append("<div id='ss-consistency' class='summary  right' />");
+		jQuery("#ss-consistency").append("<h3>Secondary Structure composition</h3>");
+		PIE_CHART.toPieData(mainObj.getSSComposition()).drawPieChart('ss-consistency');
+	};
+
 
 	return {
-		drawSubcellLoc: function() {
-			var nav_div = jQuery("<div id='_subcell_nav' />").appendTo(jQuery("#cell"));
-
-			var content_div = jQuery("<div id='_subcell_cntnt'/>").appendTo(jQuery("#cell"));
-			var list = jQuery('<ul/>');
-			list.addClass("nav nav-pills");
-
-
-			list.append('<li class="disabled"><a href="#">Domains:</a> </li>')
-
-			var domains = ["arch", "bact", "euka", "plant", "animal", "proka"];
-			for (var i in domains) {
-				var _curr_div;
-				var _curr_li = jQuery('<li><a href=#>' + SUBCELL_VIEW.getDomainFullName(domains[i]) + '</a></li>');
-				var prediction = mainObj.getSubCellLocations(domains[i]);
-				if (!prediction) _curr_div = jQuery("<div />").text("Data unavailable");
-				else  _curr_div = SUBCELL_VIEW.localisationDiv(prediction);
-
-				if (domains[i] == 'euka') {
-					_curr_div.show();
-					_curr_li.addClass('active');
-				} else _curr_div.hide();
-				list.append(_curr_li);
-				content_div.append(_curr_div);
-			}
-			nav_div.append(list);
-		},
-
-		drawAlignmentTable: function() {
-			ALI_VIEW.draw(mainObj.getAlignmentLocations(), jQuery("#alignments"));
-		},
-
-		drawSummaryTable: function() {
-			// TODO move modal activation code from this control
-			jQuery("#summary_container").append("<div class='summary  left' />");
-			jQuery(".summary").append("<h3>Summary</h3>");
-			var table = jQuery("<table/>");
-			table.addClass("table table-striped");
-			if (_rec_name = mainObj.getAlignmentsByDatabaseTopMatch('Swiss-Prot'))
-				table.append("<tr><td>Recommended Name</td><td>" + _rec_name + "</td></tr>");
-			table.append("<tr><td>Sequence Length</td><td>" + mainObj.getSequence().length + "</td></tr>");
-			table.append("<tr><td>Number of Aligned Proteins</td><td><a href='#myModal' role='button' data-toggle='modal'>" + mainObj.getAlignmentsCount() + "</a></td></tr>");
-			table.append("<tr><td>Number of Matched PDB Structures</td><td>" + mainObj.getAlignmentsByDatabase('pdb') + "</td></tr>");
-
-
-			//<a href="#myModal" role="button" class="btn" data-toggle="modal">Launch demo modal</a>
-
-			jQuery(".summary").append(table);
-		},
-
-		drawAAConsistency: function() {
-
-			jQuery("#summary_container").append("<div id='aa-consistency' class='summary  left' />");
-			jQuery("#aa-consistency").append("<h3>Amino Acid composition</h3>");
-			PIE_CHART.toPieData(mainObj.getAAComposition()).drawPieChart('aa-consistency');
-		},
-
-		drawSSConsistency: function() {
-
-			jQuery("#summary_container").append("<div id='ss-consistency' class='summary  right' />");
-			jQuery("#ss-consistency").append("<h3>Secondary Structure composition</h3>");
-			PIE_CHART.toPieData(mainObj.getSSComposition()).drawPieChart('ss-consistency');
-		},
-
-		drawFeatureViewer: function() {
-			FEATURE_VIEWER.init({
-				targetDiv: jQuery("#FeatureViewer"),
-				dataObj: mainObj
-			});
-
-			var features_array = [];
-			FEATURE_VIEWER.setFeaturesArray(jQuery.map(providers, function(provider, index) {
-				var track, feature_properties;
-				track = new Track();
-				if (provider == "ISIS") track.setShiftBottomLine(Track.NO_BOTTOMLINE_SHIFT);
-				else track.setPosition(FEATURE_VIEWER.getCurrentBottom());
-
-				var feature_group = mainObj.getFeatureByProvider(mainObj.getFeatureTypeGroup(), provider);
-				if (!feature_group) return null;
-				var feature_type = (feature_group.type) ? feature_group.type : "";
-				feature_properties = mainObj.getFeatureLocations(feature_group);
-
-
-				if (!feature_properties) return null;
-				if (feature_properties) i = feature_properties.length;
-				while (i--) {
-					var feature;
-					if (typeof Feature[provider] !== 'undefined') {
-						Feature[provider].prototype = new Feature();
-						feature = new Feature[provider](feature_properties[i], provider, feature_type);
-					} else {
-						feature = new Feature().init(feature_properties[i], provider, feature_type);
-					}
-					track.addFeature(feature);
-				}
-				FEATURE_VIEWER.addTrack(track);
-				return track.getTrack();
-			}));
-
-
-			// Add alignment
-			FEATURE_VIEWER.setFeaturesArray(jQuery.map(mainObj.getAlignmentLocations(), function(target, index) {
-				var track = new Track(1, 1);
-				track.setPosition(FEATURE_VIEWER.getCurrentBottom());
-				Feature.Alignment.prototype = new Feature();
-				feature = new Feature.Alignment(target, 'blast', 'alignmnet');
-				track.addFeature(feature);
-				FEATURE_VIEWER.addTrack(track);
-
-				return track.getTrack();
-				//iterate through array or object
-			}));
-
-
-
-			FEATURE_VIEWER.draw();
-		},
 
 		populateData: function(data) {
 			json = jQuery.xml2json(data);
@@ -154,18 +146,19 @@ var APP = (function() {
 		init: function() {
 			jQuery.noConflict(); // recommended to avoid conflict wiht other libs
 			NAVIGATION.setActiveItem(2);
+
 			NAVIGATION.show(jQuery("#nav"));
+			listener = new Listeners();
+			listener.setUp();
+
+
+
 			ds = new dataSource(file_specs);
 			result = ds.loadData();
 
 			result.done([
 			this.populateData,
-			this.drawFeatureViewer,
-			this.drawSummaryTable,
-			this.drawAAConsistency,
-			this.drawSSConsistency,
-			this.drawSubcellLoc,
-			this.drawAlignmentTable]);
+			this.showDashBoard]);
 		},
 		toggleDebug: function() {
 			debug = !debug;
@@ -174,6 +167,14 @@ var APP = (function() {
 			return mainObj;
 		},
 
+		showDashBoard: function() {
+			drawFeatureViewer();
+			drawSummaryTable();
+			drawAAConsistency();
+			drawSSConsistency();
+			drawSubcellLoc();
+			drawAlignmentTable();
+		},
 		dump: function() {
 			console.log(ds.getData());
 			console.log(mainObj.getAlignmentLocations());
@@ -194,11 +195,37 @@ var APP = (function() {
 				console.log('<h1>' + v + '</h1>');
 				console.log((JSON.stringify(mainObj.getFeatureLocations(feature))));
 			});
+		},
+		showPage: function(action, target_div) {
+			if (!target_div) target_div = jQuery("#msg");
+			switch (action) {
+				case 'tmh':
+					break;
+				case 'dash':
+					this.showDashBoard();
+					break;
+				default:
+					target_div.html(action);
+			}
+
 		}
 	};
 
 })();
 
+
+
+function Listeners() {
+	return {
+		setUp: function() {
+			jQuery(".nav-link").click(function() {
+				APP.showPage(jQuery(this).parent().attr('id'));
+				console.log(jQuery(this).parent().attr('id'));
+				return false;
+			});
+		}
+	}
+}
 
 // App Class
 // OBsolete
--- view/ppres.navigation.js
+++ view/ppres.navigation.js
@@ -1,56 +1,80 @@
 var NAVIGATION = (function() {
 
 	var navigation_items = {
-		"views": ["Dashborad"],
-		"detailed predictions": 
-		["Secondary Structure", 
-		"Transmembrane Regions",
-		"Non-Regular Secondary Structure",
-		"Protein Disorder and Flexibility",
-		"Disulphide and Metal Binding",
-		"Binding Sites",
-		"Subcellular Localization",
-		"Transmembrane Betta-barrels"
-		],
-		"Further Analysis": [
-		"Functional Changes",
-		"Literature Search"
-		],
+		"views": [
+		{
+			id: "dash",
+			text: "Dashborad"
+		}],
+		"detailed predictions": [
+		{
+			text: "Secondary Structure",
+			id: "secstruct"
+		}, {
+			id: "thm",
+			text: "Transmembrane Regions"
+		}, {
+			id: "nors",
+			text: "Non-Regular Secondary Structure"
+		}, {
+			id: "disorder",
+			text: "Protein Disorder and Flexibility"
+		}, {
+			id: "disulphide",
+			text: "Disulphide and Metal Binding"
+		}, {
+			id: "binding",
+			text: "Binding Sites"
+		}, {
+			id: "subcell",
+			text: "Subcellular Localization"
+		}, {
+			id: "tmb",
+			text: "Transmembrane Betta-barrels"
+		}],
+		"Further Analysis": [ 
+			{
+				id: "func",
+				text: "Functional Changes",
+			}, {
+				id: "lit",
+				text: "Literature Search"
+			}],
 
-	},active_item=2;
-	var list_root = jQuery('<ul>');
-	for (var key in navigation_items) {
-		var item = jQuery('<li>');
-		item.addClass("nav-header").text(key);
-		list_root.append(item);
-		var arr = navigation_items[key];
-		for (var prop in arr) {
-			item = jQuery('<li>');
-			var link = jQuery('<a>', {
-				text: arr[prop],
-				title:  arr[prop],
-				href: '#'
-			});
-			link.append(jQuery("<i/>").addClass("icon-chevron-right"));
-			item.append(link);
-			list_root.append(item);
-		}
-	}
-	list_root.addClass("nav nav-list bs-docs-sidenav");
-	jQuery( "li:nth-child(2)"  ,jQuery(list_root)).addClass("active");	
-	
-
-
-	return {
-		show :function ( target_div ){
-			target_div.append(list_root);		
 		},
-		setActiveItem: function( _item_number ){
-			jQuery('.active',  jQuery(list_root)).removeClass('active');
-			var _item_selector = "li:nth-child("+_item_number+")";
-			jQuery( _item_selector  ,jQuery(list_root)).addClass("active");	
-			// _item_number = active_item;
+		active_item = 2;
+		var list_root = jQuery('<ul>');
+		for (var key in navigation_items) {
+			var item = jQuery('<li>');
+			item.addClass("nav-header").text(key);
+			list_root.append(item);
+			var arr = navigation_items[key];
+			for (var prop in arr) {
+				item = jQuery('<li>');
+				item.attr('id',arr[prop].id)
+				var link = jQuery('<a>', {
+					text: arr[prop].text,
+					title: arr[prop].text,
+					href: '#'
+				});
+				link.addClass("nav-link");
+				link.append(jQuery("<i/>").addClass("icon-chevron-right"));
+				item.append(link);
+				list_root.append(item);
+			}
 		}
-	}
+		list_root.addClass("nav nav-list bs-docs-sidenav");
+		jQuery("li:nth-child(2)", jQuery(list_root)).addClass("active");
+		return {
+			show: function(target_div) {
+				target_div.append(list_root);
+			},
+			setActiveItem: function(_item_number) {
+				jQuery('.active', jQuery(list_root)).removeClass('active');
+				var _item_selector = "li:nth-child(" + _item_number + ")";
+				jQuery(_item_selector, jQuery(list_root)).addClass("active");
+				// _item_number = active_item;
+			}
+		}
 
-})();
+	})();
